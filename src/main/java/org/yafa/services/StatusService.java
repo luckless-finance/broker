e6d2e59d0476704e4375548ce65ccb87c48e50b2
gcrowell <gcrowell@venacorp.com>
2021-05-04 23:06:19 -0400

    logging

--- src/main/java/org/yafa/services/StatusService.java
+++ src/main/java/org/yafa/services/StatusService.java
@@ -1,11 +1,17 @@
 package org.yafa.services;
 
 import javax.enterprise.context.Dependent;
+import lombok.extern.slf4j.Slf4j;
 import org.yafa.api.dto.outbound.ServerStatus;
 
+@Slf4j
 @Dependent
 public class StatusService {
   public ServerStatus getStatus() {
+    log.debug("debug");
+    log.info("info");
+    log.warn("warn");
+    log.error("error");
     return ServerStatus.builder().status(ServerStatus.Status.OK).build();
   }
 }
